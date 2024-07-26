565c6c8081e6e99a99c6d2f16bea9444e7ad12a0
ElizamaHeloize <elizamaheloize008@gmail.com>
2024-07-26 13:41:05 +0000

    prova 1

--- lab04/Jogo.java
+++ lab04/Jogo.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 public class Jogo {
     // java lab04/Jogo.java
     public static void main(String[] args) {
-        /*/ CRIAR JOGO QUE GERE UM NÚMERO ALEATORIO  */
+        /*/ CRIAR JOGO QUE GERE UM NÚMERO ALEATORIO */
+
         var sc = new Scanner(System.in);
             System.out.println("Informe seu número da sorte: ");
-        var numero = 0;
             numero = sc.nextInt();
-            while (numero < 2){
-                if (numero == 2){
+            int aleatorio = int 
+            while (numero < 100){
+                if (aleatorio = numero ){
                      System.out.println("Esse é o número da sorte! ");
                 }
                 
-                else if( numero != 10 ){
+                else if( aleatorio != numero  ){
                     System.out.println("Esse não é o número da sorte! ");
                 }
             }
